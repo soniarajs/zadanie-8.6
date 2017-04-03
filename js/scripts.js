ff44ef68ec11aac47eaea913c8cee323b29807a8
sonia <sonia.ryszytylo@gmail.com>
2017-04-04 00:06:45 +0200

    funkcjee

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+
+var a  = ,
+    h  = ;
+
+function getTriangleArea (a, h) {
+
+        a*h/2
+
+
+	if (a > 0) {
+	console.log('Dane ok');
+	}
+	if (h > 0) {
+	console.log('Dane ok');
+	}
+	else {
+	console.log('Nieprawidłowe dane');
+	}
+
+	return 'wartośćDzialania';
+	var wartośDzialania = getTriangleArea(a, h);
+}
+
+console.log(getTriangleArea(10,6))
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(7, 1);
+var triangle3Area = getTriangleArea(-4, 0);
