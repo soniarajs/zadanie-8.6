30b8b4209e51e83ad67c2cff5de3385cd3dbd3ba
sonia <sonia.ryszytylo@gmail.com>
2017-04-04 00:20:14 +0200

    zmiany

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,22 @@
 
-var a  = ,
-    h  = ;
+var a,
+	h;
 
 function getTriangleArea (a, h) {
 
-        a*h/2
-
-
-	if (a > 0) {
-	console.log('Dane ok');
-	}
-	if (h > 0) {
-	console.log('Dane ok');
+	if  ( (a > 0) && (h > 0) ) {
+	//console.log('Dane ok');
+	wartośćDzialania = a*h/2;
 	}
 	else {
 	console.log('Nieprawidłowe dane');
 	}
 
 	return 'wartośćDzialania';
-	var wartośDzialania = getTriangleArea(a, h);
+	var wartośćDzialania = getTriangleArea(a, h);
 }
 
-console.log(getTriangleArea(10,6))
+console.log( getTriangleArea(10,6) )
 
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(7, 1);
