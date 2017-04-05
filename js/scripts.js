01ddb2ac4ec7de3340e889c0568b535f7b3ad4c9
sonia <sonia.ryszytylo@gmail.com>
2017-04-05 08:35:08 +0200

    poprawione

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
 
-var a,
-	h;
+var a, h;
 
 function getTriangleArea (a, h) {
+	var wartoscDzialania;
 
 	if  ( (a > 0) && (h > 0) ) {
-	//console.log('Dane ok');
-	wartośćDzialania = a*h/2;
-	}
-	else {
-	console.log('Nieprawidłowe dane');
+			wartoscDzialania = a*h/2;
+	} else {
+			console.log('Nieprawidłowe dane');
 	}
 
-	return 'wartośćDzialania';
-	var wartośćDzialania = getTriangleArea(a, h);
+	return wartoscDzialania;
 }
 
-console.log( getTriangleArea(10,6) )
+console.log( getTriangleArea(10, 6) )
 
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(7, 1);
 var triangle3Area = getTriangleArea(-4, 0);
+
+consoloe.log(triangle1Area, triangle2Area, triangle3Area);
